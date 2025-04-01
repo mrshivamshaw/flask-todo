c28f4ee573fff387c9bd8b4a5cef589d574a8439
SHIVAM KUMAR SHAW <108209696+mrshivamshaw@users.noreply.github.com>
2025-04-01 16:50:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-
 # /// = relative path, //// = absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
+# Create tables if they don't exist
+with app.app_context():
+    db.create_all()
 
 @app.route("/")
 def home():
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
-
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -29,14 +30,12 @@ def add():
     db.session.commit()
     return redirect(url_for("home"))
 
-
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
-
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
@@ -46,5 +45,5 @@ def delete(todo_id):
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
